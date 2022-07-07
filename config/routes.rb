5fdfae630339e8c8ba23ae115c7f994d202bbc6a
abubakar055 <abubakarnawaz055@gmail.com>
2022-07-07 14:41:40 +0500

    implemented authentication using devise for bugzilla

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, path: '', path_names: {sign_in: "login",sign_up: "register",sign_out: "logout"}
+  
+  ##get 'pages/home'
+  get 'pages/about'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+
+  root to: "pages#home"
 end
